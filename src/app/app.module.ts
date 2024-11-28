c044e67c9dbded87364b412ba032ff14ceb2d406
Dayane <dayane.lima@escolar.ifrn.edu.br>
2024-11-28 11:24:23 -0300

    mudanças

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import { CadastrarmedicosComponent } from './cadastrarmedicos/cadastrarmedicos.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,  
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
